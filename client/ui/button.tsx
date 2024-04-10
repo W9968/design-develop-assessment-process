56c839e3e7cf83f9d374c17c3e39a920ce9f70cd
Walee Sebiiy <waleesebiiy@gmail.com>
2024-04-10 03:40:47 +0100

    update button props

--- client/ui/button.tsx
+++ client/ui/button.tsx
@@ -1,19 +1,21 @@
-import type { FC, ButtonHTMLAttributes } from 'react'
+import type { ButtonHTMLAttributes, FC, ReactElement } from 'react'
 
 import { mr } from '@/utils/class-authority-merge'
 import { cva, type VariantProps } from 'class-variance-authority'
 
 const buttonVariant = cva(
-  'w-full flex items-center justify-center border border-content-display pointer-cursor disabled:bg-gray-200 disabled:border-gray-200 disabled:text-content-disabled disabled:pointer-events-none rounded',
+  'w-full flex items-center justify-center border-[2px] border-content-display pointer-cursor disabled:bg-gray-200 disabled:border-gray-200 disabled:text-content-disabled disabled:pointer-events-none rounded',
   {
     variants: {
       variant: {
         default: 'bg-primary-yellow text-primary-background text-content-display hover:bg-primary-black hover:text-primary-white',
         primary: 'bg-content-display text-primary-white hover:bg-primary-white hover:text-content-display',
         secondary: 'bg-primary-white text-content-display hover:bg-primary-black hover:text-primary-white',
-        link: 'text-accent-link border border-transparent hover:text-blue-500 hover:bg-gray-200',
-        ghost: 'bg-transparent border border-transparent text-content-display hover:bg-gray-200',
-        error: 'bg-accent-error border border-accent-error text-primary-white hover:bg-red-500',
+        tertiary: 'bg-purple-400 border-[2px] border-purple-700 text-primary-white hover:bg-purple-500',
+        info: 'bg-accent-link border-[2px] border-blue-700 text-primary-white hover:bg-blue-500',
+        link: 'text-accent-link border-[2px] border-transparent hover:text-blue-500 hover:bg-gray-200',
+        ghost: 'bg-transparent border-[2px] border-transparent text-content-display hover:bg-gray-200',
+        error: 'bg-accent-error border-[2px] border-red-700 text-primary-white hover:bg-red-500',
       },
       size: {
         default: 'h-[40px] px-8',
@@ -28,9 +30,11 @@ const buttonVariant = cva(
   }
 )
 
-interface ComponentProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariant> {}
+interface ComponentProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantProps<typeof buttonVariant> {
+  icon?: ReactElement
+}
 
-export const Button: FC<ComponentProps> = ({ title, variant = 'default', size = 'default', className, ...rest }) => (
+export const Button: FC<ComponentProps> = ({ title, icon, variant = 'default', size = 'default', className, ...rest }) => (
   <button
     className={mr(
       buttonVariant({
@@ -40,6 +44,7 @@ export const Button: FC<ComponentProps> = ({ title, variant = 'default', size = 
       })
     )}
     {...rest}>
+    {icon && icon}
     <p className={mr('text-sm font-[550] tracking-wide capitalize')}>{title}</p>
   </button>
 )
