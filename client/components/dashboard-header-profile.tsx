984f91197467ff6d4cfc3445912a934134a48e53
Walee Sebiiy <waleesebiiy@gmail.com>
2024-04-19 11:35:36 +0100

    update auth methode

--- client/components/dashboard-header-profile.tsx
+++ client/components/dashboard-header-profile.tsx
@@ -33,7 +33,7 @@ export const DashboardHeaderProfile: FC<ComponentProps> = () => {
         ) : (
           <button className='w-full flex-1 h-full cursor-pointer flex items-center gap-3 select-none' onClick={() => push('/dashboard/profile')}>
             {/* eslint-disable-next-line @next/next/no-img-element */}
-            <img src='https://api.dicebear.com/8.x/bottts-neutral/svg?seed=Sophie' alt='' className='h-10 w-10 object-cover flex rounded-full' />
+            <img src={user.profileImage ? user.profileImage : 'https://api.dicebear.com/8.x/bottts-neutral/svg?seed=Sophie'} alt='' className='h-10 w-10 object-cover flex rounded-full' />
             <div className={'flex flex-1 flex-col items-start px-2'}>
               <p className='text-primary-white text-base capitalize'>{`${user.firstName} ${user?.middleName} ${user.lastName}`}</p>
               <p className='text-gray-300 text-sm'>{user.username}</p>
