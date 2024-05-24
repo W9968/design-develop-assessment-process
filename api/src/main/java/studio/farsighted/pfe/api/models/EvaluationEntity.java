3c033d1a3f375c3d4c70d80a6b089af52a99fc62
Walee Sebiiy <waleesebiiy@gmail.com>
2024-05-24 10:49:16 +0100

    creating the EvaluationController period between startup and cohorts

--- api/src/main/java/studio/farsighted/pfe/api/models/EvaluationEntity.java
+++ api/src/main/java/studio/farsighted/pfe/api/models/EvaluationEntity.java
@@ -1,2 +1,35 @@
-package studio.farsighted.pfe.api.models;public class EvaluationEntity {
+package studio.farsighted.pfe.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "datatable-evaluations")
+public class EvaluationEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "evaluation-score")
+    private Integer evaluationScore = 0;
+
+    @Column(name = "evaluation-feedback", columnDefinition = "TEXT")
+    private String evaluationFeedback;
+
+    @ManyToOne()
+    @JoinColumn(referencedColumnName = "id")
+    private ProgramCohortEntity cohort;
+
+    @ManyToOne()
+    @JoinColumn(referencedColumnName = "id")
+    private StartupEntity startup;
+
 }
